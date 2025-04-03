d837819d0983978b020a84834570e809fdcc3962
Nikolay Larsen <nikolay@velkomb.com>
2025-04-03 10:34:27 +0500

    chore: update dependencies

--- src/components/RoundCell.vue
+++ src/components/RoundCell.vue
@@ -37,7 +37,7 @@ function onClick() {
 </script>
 <template>
   <div
-    class="relative flex aspect-[0.92/1] max-w-[calc((100%_-_24px)_/_5)] flex-grow select-none items-center justify-center rounded-md border text-3xl leading-[62px] sm:text-xl"
+    class="relative flex aspect-[0.92/1] max-w-[calc((100%_-_24px)_/_5)] grow select-none items-center justify-center rounded-md border text-3xl sm:text-xl"
     :class="{
       'border-[#ffdd2d]': status === LetterStatus.DEFAULT,
       'border-[#5f5f5f] bg-[#5f5f5f]': status === LetterStatus.NOT_IN_WORD,
